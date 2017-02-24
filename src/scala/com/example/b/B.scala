35ae7d22bcdfce3e48764bf097c806109ca637bb
Yujie Chen <yujiec@twitter.com>
2017-02-24 12:36:07 -0500

    some commits

--- src/scala/com/example/b/B.scala
+++ src/scala/com/example/b/B.scala
@@ -1,9 +1,9 @@
 package com.example.b
 
-import com.example.a.A
+//import com.example.a.A
 
 trait B {
-  def create: A.type = A
+  def create: com.foo.a.A.type = com.foo.a.A
 
   def empty: String
 }
