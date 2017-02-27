3dfa74c9215dbe085dcac8a4c7634c26839ae416
Yujie Chen <yujiec@twitter.com>
2017-02-27 11:01:51 -0500

    create some error case

--- src/scala/com/example/b/B.scala
+++ src/scala/com/example/b/B.scala
@@ -3,7 +3,7 @@ package com.example.b
 //import com.example.a.A
 
 trait B {
-  def create: com.foo.a.A.type = com.foo.a.A
+  def create: com.example.a.A.type = com.example.a.A
 
   def empty: String
 }
